ba903aff1eabfb77f2949ba48969941e1f63054d
baijin <2368276566@qq.com>
2018-04-10 15:29:34 +0800

    fix :add question router and controller

--- controllers/question.controller.js
+++ controllers/question.controller.js
@@ -1,14 +1,39 @@
 var Question = require('mongoose').model("question")
 var utils = require('../utils/utils');
 
-exports.saveQuestionList = function (req, res, next) {
-    var Question = new Question(req.body);
-    Question.save()
-        .then(() => {
-            utils.abortAndSendJsonToClient(null,req,res,{_id: Question._id})
+exports.findQuestionList = function (req, res, next) {
+    var {page_number,page_size} = req.body;
+    page_number || (page_number = 0);
+    page_size || (page_size = 10);
+    Question.find().skip(page_number*page_size).limit(page_size)
+        .then((data) => {
+            utils.abortAndSendJsonToClient(null, req, res, data)
         })
         .catch(err => {
-            utils.abortAndSendJsonToClient(err,req,res,null)
+            utils.abortAndSendJsonToClient(err, req, res, null)
         })
-}
+};
 
+exports.saveQuestionList = function (req, res, next) {
+    var question = new Question(req.body);
+    question.save()
+        .then(() => {
+            utils.abortAndSendJsonToClient(null, req, res, {_id: Question._id})
+        })
+        .catch(err => {
+            utils.abortAndSendJsonToClient(err, req, res, null)
+        })
+};
+
+exports.deleteQuestionList = function (req, res, next) {
+    if (!req.body.listId) {
+        utils.abortAndSendJsonToClient(new Error("List id is null"), req, res, null)
+    }
+    Question.remove({_id: req.body.listId})
+        .then(() => {
+            utils.abortAndSendJsonToClient(null, req, res, {_id: Question._id})
+        })
+        .catch(err => {
+            utils.abortAndSendJsonToClient(err, req, res, null)
+        })
+};
--- routes/question.js
+++ routes/question.js
@@ -3,6 +3,9 @@ var router = express.Router();
 var question = require("../controllers/question.controller");
 
 router.route("/saveQuestionList")
-    .post(question.saveQuestionList)
-
+    .post(question.saveQuestionList);
+router.route("/deleteQuestionList")
+    .post(question.deleteQuestionList);
+router.route("/findQuestionList")
+    .post(question.findQuestionList);
 module.exports = router;
